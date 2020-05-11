b3345e041ab1ea496972f5278efd850ae80b912d
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-05-10 22:53:38 -0700

    Add user auth

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const mongoose = require('mongoose')
-
 const app = express();
 const db = require('./config/keys').mongoURI
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser')
+const passport = require('passport');
+require('./config/passport')(passport);
+
+
 
 
 mongoose
@@ -10,8 +16,12 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World24"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use(passport.initialize());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 
 const port = process.env.PORT || 5000;
